30eec369fd6d84368f35dc6d13589f0fd1c31be9
Sergio <sergioruza22@gmail.com>
2023-07-27 11:31:28 -0300

    feat: install and configure eslint

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-const a: string = 'a'
-console.log(a);
+import express from 'express';
 
+const server = express();
+
+server.get('/', (req, res) => res.send('hello'));
+
+server.listen(3333, () => {
+  console.log('Server started on port 3333');
+});

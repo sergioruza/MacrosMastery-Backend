32024a6109c693ad146a8c1a70fc7cbb85d6278a
Sergio <sergioruza22@gmail.com>
2023-07-31 16:55:10 -0300

    feat: migration users

--- src/database/config/connection.ts
+++ src/database/config/connection.ts
@@ -3,8 +3,8 @@ import { Options } from 'sequelize';
 
 const config: Options = {
   username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '123456',
-  database: 'TRYBE_FUTEBOL_CLUBE',
+  password: process.env.DB_PASS || 'password',
+  database: 'Macros_Mastery_db',
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 3002,
   dialect: 'mysql',
